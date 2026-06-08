d342d890c4b90b8ca4cfe2b74d6ad981290db23b
chris <owcng2021@gmail.com>
2026-06-08 11:19:00 -0600

    Add actor and repository models

---
+++ app/models/actor.rb
@@ -0,0 +1,9 @@
+class Actor < ApplicationRecord
+  validates :github_id, presence: true, uniqueness: true
+
+  has_many :push_events, primary_key: :github_id, foreign_key: :actor_id, dependent: :nullify
+
+  def fresh?(ttl)
+    fetched_at.present? && fetched_at > ttl.ago
+  end
+end
